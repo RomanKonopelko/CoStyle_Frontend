d64a6b01cc6e9df07babc0536cf07157ff6070ae
AndriiKulba <avk.0149344@gmail.com>
2021-07-06 14:51:24 +0300

    add Modal and ButtonAddTransaction

--- src/views/DashboardPage.js
+++ src/views/DashboardPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectorsAuth, ActionAuth } from '../redux/auth';
 import Header from '../components/Header';
 import Navigation from '../components/Navigation/Navigation';
 
@@ -6,8 +8,11 @@ import HomeTab from '../components/HomeTab/HomeTab';
 
 import Balance from '../components/Balance';
 import Currency from '../components/Currency';
+import ButtonAddTransaction from '../components/ButtonAddTransactions/ButtonAddTransactions';
+import Modal from '../components/Shared/Modal';
 
 export default function DashboardPage() {
+  const isShowModal = useSelector(selectorsAuth.getShowModal);
   return (
     <>
       <Header />
@@ -19,6 +24,8 @@ export default function DashboardPage() {
         <p>Якщо вкладка "Головна" то: Currency, Balance і HomeTab</p>
         <p> Якщо вкладка "Статистика": DiagramTab (Chart і Table) </p>
       </div>
+      <ButtonAddTransaction />
+      {isShowModal && <Modal />}
     </>
   );
 }
