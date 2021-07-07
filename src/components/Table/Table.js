dcbb3057f9f969909bc17267e212dec965232b58
Svetlana Khvastova <svetakhvastova@gmail.com>
2021-07-07 17:25:23 +0300

    preliminarily/Table_api

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -9,26 +9,9 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import moment from 'moment';
 
-// для примера захардкодила значения
-// логику нужно еще писать
-const API_DATA = [
-  {
-    category: 'Основные расходы',
-    summ: '8 700.00',
-    month: 'Jan',
-    year: '2020',
-  },
-  { category: 'Продукты', summ: '3 800.74', month: 'Feb', year: '2022' },
-  { category: 'Машина', summ: '1 500.00', month: 'Dec', year: '2021' },
-  { category: 'Забота о себе', summ: '800.00', month: 'Jul', year: '2021' },
-  { category: 'Забота о детях', summ: '2208.50', month: 'Jan', year: '2021' },
-  { category: 'Товары для дома', summ: '300.00', month: 'Jun', year: '2021' },
-  { category: 'Образование', summ: '3 400.00', month: 'Aug', year: '2020' },
-  { category: 'Досуг', summ: '1 230.00', month: 'May', year: '2020' },
-  { category: 'Другие расходы', summ: '610.00', month: 'Aug', year: '2020' },
-];
-
+// предварительные стили
 const useStyles = makeStyles(theme => ({
   formControl: {
     margin: theme.spacing(1),
@@ -43,18 +26,33 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-function MyTable() {
+function MyTable({ transactions }) {
   const classes = useStyles();
-  const [tableData, setTableData] = React.useState(API_DATA);
+
+  const [tableData, setTableData] = React.useState(transactions);
   const [selected, setSelected] = React.useState('');
 
   function handleChange(event) {
     setSelected(event.target.value);
-    console.log(event.target.value);
-    let _vals = event.target.value
-      ? API_DATA.filter(r => r.month === event.target.value)
-      : API_DATA;
-    setTableData(_vals);
+    console.log(`event.target.value`, event.target.value);
+
+    const tableMonthFilter = tableData.filter(r => {
+      const myMonth = moment(r.time, 'DD.MM.YYYY').locale('ru').format('MMMM');
+      console.log(`myMonth`, myMonth);
+      return myMonth;
+    });
+
+    const tableYearFilter = tableData.filter(r => {
+      const myYear = moment(r.time, 'DD.MM.YYYY').locale('ru').format('YYYY');
+      console.log(`myYear`, myYear);
+      return myYear;
+    });
+
+    // что-то тут не так ((
+    // let _vals = event.target.value
+    //   ? tableMonthFilter === event.target.value
+    //   : tableData;
+    // setTableData(_vals);
   }
 
   return (
@@ -66,27 +64,32 @@ function MyTable() {
           displayEmpty
           onChange={handleChange}
         >
-          <MenuItem value="">
+          <MenuItem value={selected}>
             <em>Месяц</em>
           </MenuItem>
-          <MenuItem value="Jan">Январь</MenuItem>
-          <MenuItem value="Feb">Февраль</MenuItem>
-          <MenuItem value="Mar">Март</MenuItem>
-          <MenuItem value="Apr">Апрель</MenuItem>
+          <MenuItem value="January">Январь</MenuItem>
+          <MenuItem value="February">Февраль</MenuItem>
+          <MenuItem value="March">Март</MenuItem>
+          <MenuItem value="April">Апрель</MenuItem>
           <MenuItem value="May">Май</MenuItem>
-          <MenuItem value="Jun">Июнь</MenuItem>
-          <MenuItem value="Jul">Июль</MenuItem>
-          <MenuItem value="Aug">Август</MenuItem>
-          <MenuItem value="Sept">Сентябрь</MenuItem>
-          <MenuItem value="Oct">Октябрь</MenuItem>
-          <MenuItem value="Nov">Ноябрь</MenuItem>
-          <MenuItem value="Dec">Декабрь</MenuItem>
+          <MenuItem value="June">Июнь</MenuItem>
+          <MenuItem value="July">Июль</MenuItem>
+          <MenuItem value="August">Август</MenuItem>
+          <MenuItem value="September">Сентябрь</MenuItem>
+          <MenuItem value="October">Октябрь</MenuItem>
+          <MenuItem value="November">Ноябрь</MenuItem>
+          <MenuItem value="December">Декабрь</MenuItem>
         </Select>
       </FormControl>
 
       <FormControl variant="outlined" className={classes.formControl}>
-        <Select value={''} name="year" displayEmpty onChange={handleChange}>
-          <MenuItem value="">
+        <Select
+          value={selected}
+          name="year"
+          displayEmpty
+          onChange={handleChange}
+        >
+          <MenuItem value={selected}>
             <em>Год</em>
           </MenuItem>
           <MenuItem value="2020">2020</MenuItem>
@@ -106,9 +109,9 @@ function MyTable() {
 
         <TableBody>
           {tableData.map(row => (
-            <TableRow key={row.category}>
+            <TableRow key={row.id}>
               <TableCell align="left">{row.category}</TableCell>
-              <TableCell align="left">{row.summ}</TableCell>
+              <TableCell align="left">{Number(row.amount)}</TableCell>
             </TableRow>
           ))}
 
