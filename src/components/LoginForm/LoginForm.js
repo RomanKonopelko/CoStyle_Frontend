3944ce6c20589626c0c6229f32a2ca2b7e378f29
Svetlana Khvastova <svetakhvastova@gmail.com>
2021-07-09 20:35:08 +0300

    add comment password

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -22,7 +22,7 @@ const validationSchema = yup.object({
   password: yup
     .string('Enter your password')
     .min(6, 'Password should be of minimum 6 characters length')
-    .max(12, 'Password should be of maximum 8 characters length')
+    .max(12, 'Password should be of maximum 12 characters length')
     .required('Password is required'),
 });
 
