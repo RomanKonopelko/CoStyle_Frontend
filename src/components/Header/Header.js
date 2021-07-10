4aea9c70e3f932a196536eceafff70464061c386
AndriiKulba <avk.0149344@gmail.com>
2021-07-10 18:23:21 +0300

    header css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,9 @@
 import { useSelector } from 'react-redux';
 import ModalLogout from '../ModalLogout/ModalLogout';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import headerLogo from './header-logo.png';
 import { selectorsAuth } from '../../redux/auth';
 import Avatar from '@material-ui/core/Avatar';
+import Container from '../Container/Container';
 import './header.scss';
 
 export default function Header() {
@@ -11,24 +11,32 @@ export default function Header() {
 
   return (
     <header className="header">
-      <img className="img" src={headerLogo} alt="logo" />
-      <h1 className="title">Wallet</h1>
-      <Avatar
-        style={{
-          width: '30px',
-          height: '30px',
-          fontSize: '16px',
-          backgroundColor: '#4a56e2',
-          fontWeight: 'normal',
-          fontStyle: 'italic',
-        }}
-      >
-        {name.substr(0, 1)}
-      </Avatar>
-      <p className="name">{name}</p>
-      <span className="decor"></span>
-      <ExitToAppIcon className="icon" />
-      <ModalLogout />
+      <Container>
+        <div className="headerContainer">
+          <div className="logo">
+            <img className="img" src={headerLogo} alt="logo" />
+            <h1 className="title">Wallet</h1>
+          </div>
+          <div className="userInfo">
+            <Avatar
+              style={{
+                width: '30px',
+                height: '30px',
+                fontSize: '16px',
+                backgroundColor: '#4a56e2',
+                fontWeight: 'normal',
+                fontStyle: 'italic',
+              }}
+            >
+              {name.substr(0, 1)}
+            </Avatar>
+            <p className="name">{name}</p>
+            <span className="decor"></span>
+
+            <ModalLogout />
+          </div>
+        </div>
+      </Container>
     </header>
   );
 }
