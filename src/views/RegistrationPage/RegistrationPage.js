ae3a9942cb960539dbfffaede9d5572911c6d5ba
Huracan <huracan22505@protonmail.com>
2021-07-11 14:10:11 +0300

    refactor: common sass

--- src/views/RegistrationPage/RegistrationPage.js
+++ src/views/RegistrationPage/RegistrationPage.js
@@ -1,22 +1,25 @@
 import React from 'react';
+import Container from '../../components/Container/Container';
 import RegistrationForm from '../../components/RegistrationForm/';
 
 import img from '../../images/Frame.png';
 
 export default function RegistrationPage() {
   return (
-    <div className="mainPage">
-      <div className="split left">
-        <div className="centred">
-          <img src={img} alt="finance app" className="imgApp" />
-          <h1 className="financeApp">Finance App</h1>
+    <Container>
+      <div className="mainPage">
+        <div className="split left">
+          <div className="centred">
+            <img src={img} alt="finance app" className="imgApp" />
+            <h1 className="financeApp">Finance App</h1>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centred">
+            <RegistrationForm />
+          </div>
         </div>
       </div>
-      <div className="split right">
-        <div className="centred">
-          <RegistrationForm />
-        </div>
-      </div>
-    </div>
+    </Container>
   );
 }
