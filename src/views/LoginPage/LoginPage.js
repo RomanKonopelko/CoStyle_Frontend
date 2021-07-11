8d98d4651aaa27fc2297942bea18c51e4fa69e13
Kateryna Lutska <ket.lutska@gmail.com>
2021-07-11 11:14:10 +0300

    loginpage

--- src/views/LoginPage/LoginPage.js
+++ src/views/LoginPage/LoginPage.js
@@ -4,12 +4,18 @@ import img from '../../images/Frame.png';
 
 export default function LoginPage() {
   return (
-    <div className="loginPage">
-      <div>
-        <img src={img} alt="finance app" className="imgApp" />
-        <h1 className="financeApp">Finance App</h1>
+    <div className="mainPage">
+      <div className="split left">
+        <div className="centred">
+          <img src={img} alt="finance app" className="imgApp" />
+          <h1 className="financeApp">Finance App</h1>
+        </div>
       </div>
-      <LoginForm className="loginForm" />
+      <div className="split right">
+        <div className="centred">
+          <LoginForm />
+        </div>
+      </div>
     </div>
   );
 }
