57efffaa42b4c566e5141c8abbe9eda983f8aa1d
Huracan <huracan22505@protonmail.com>
2021-07-11 17:12:21 +0300

    refactor: markup for adaptive login and reg views

--- src/views/RegistrationPage/RegistrationPage.js
+++ src/views/RegistrationPage/RegistrationPage.js
@@ -6,20 +6,18 @@ import img from '../../images/Frame.png';
 
 export default function RegistrationPage() {
   return (
-    <Container>
-      <div className="mainPage">
-        <div className="split left">
-          <div className="centred">
-            <img src={img} alt="finance app" className="imgApp" />
-            <h1 className="financeApp">Finance App</h1>
+    <div className="login-page-bcgi">
+      <Container>
+        <div className="mainPage">
+          <div className="left">
+            <img src={img} alt="finance app" className="img" />
+            <h1 className="title">Finance App</h1>
           </div>
-        </div>
-        <div className="split right">
-          <div className="centred">
+          <div className=" right">
             <RegistrationForm />
           </div>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </div>
   );
 }
