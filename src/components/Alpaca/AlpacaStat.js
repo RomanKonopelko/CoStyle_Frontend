61ebb523467ca9c30758af114fa8fe9270389520
Kateryna Lutska <ket.lutska@gmail.com>
2021-07-21 15:56:40 +0300

    loader

--- src/components/Alpaca/AlpacaStat.js
+++ src/components/Alpaca/AlpacaStat.js
@@ -1,12 +1,10 @@
+import AlpacaStats from '../../images/AlpacaStat.gif';
+
 export default function AlpacaStat() {
   return (
     <>
       <div className="alpacaStat">
-        <img
-          className="alpacatImg"
-          src="https://media2.giphy.com/media/5nh30ByowIC9jVH1b6/source.gif"
-          alt="alpaca"
-        />
+        <img className="alpacatImg" src={AlpacaStats} alt="alpaca" />
       </div>
     </>
   );
